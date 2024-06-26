ccd02a7743d6ed8988fc159ff8d72034b0e8c43f
jiaao <jiaao@jiaaodeMacBook-Pro.local>
2024-06-26 10:06:08 +0800

    dev

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,28 +12,19 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- nvim-tree
-  {
-    "nvim-tree/nvim-tree.lua", 
-    version = "*",
-    lazy = false,
-    dependencies = { "nvim-tree/nvim-web-devicons" },   
-  },
-  -- -- bufferline
-  -- {"akinsho/bufferline.nvim", dependencies = { "nvim-tree/nvim-web-devicons", "moll/vim-bbye" }},
-  -- -- lualine
-  -- {"nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }},
-  -- "arkav/lualine-lsp-progress",
-  -- -- telescope
-  -- {"nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }},
-  -- -- telescope extensions
-  -- "LinArcX/telescope-env.nvim",
-  -- "nvim-telescope/telescope-ui-select.nvim",
-  -- "ahmedkhalf/project.nvim",
-  -- -- dashboard-nvim
-  -- "glepnir/dashboard-nvim",
-  -- -- Comment
-  -- "numToStr/Comment.nvim",
-  -- -- vscode
-  -- "Mofiqul/vscode.nvim",
+  -- lualine
+  {"nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }},
+  "arkav/lualine-lsp-progress",
+  -- telescope
+  {"nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }},
+  -- telescope extensions
+  "LinArcX/telescope-env.nvim",
+  "nvim-telescope/telescope-ui-select.nvim",
+  "ahmedkhalf/project.nvim",
+  -- dashboard-nvim
+  "glepnir/dashboard-nvim",
+  -- Comment
+  "numToStr/Comment.nvim",
+  -- vscode
+  "Mofiqul/vscode.nvim",
 })
