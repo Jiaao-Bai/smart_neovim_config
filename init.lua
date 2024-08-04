2c64b2378a11972062216030d76451ad1a1e910c
smart <baijiaao22@gmail.com>
2024-08-04 22:05:34 +0800

    support nvim-tree

--- init.lua
+++ init.lua
@@ -5,6 +5,8 @@ require("config.keymaps")
 -- Packer插件管理
 require("config.lazy")
 -- -- 插件配置
+require("plugin_config.nvim-tree")
+
 require("plugin_config.colorscheme")
 require("plugin_config.lualine")
 require("plugin_config.telescope")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,17 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
   -- lualine
   {"nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" }},
   "arkav/lualine-lsp-progress",
