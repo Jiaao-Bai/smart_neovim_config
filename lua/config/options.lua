9bcbc37bad81b73604f0a285f295ab5549c00038
smart <baijiaao22@gmail.com>
2024-08-11 00:11:05 +0800

    update treesitter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -91,3 +91,9 @@ vim.opt.clipboard = "unnamedplus"
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.o.background = "dark"
+-- 开启 Folding 模块
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- 默认不要折叠
+-- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
+vim.opt.foldlevel = 99
--- lua/plugin_config/nvim-treesitter.lua
+++ lua/plugin_config/nvim-treesitter.lua
@@ -17,19 +17,19 @@ treesitter.setup({
     additional_vim_regex_highlighting = false,
   },
   -- 启用增量选择模块
---   incremental_selection = {
---     enable = true,
---     keymaps = {
---       init_selection = "<CR>",
---       node_incremental = "<CR>",
---       node_decremental = "<BS>",
---       scope_incremental = "<TAB>",
---     },
---   },
+  --   incremental_selection = {
+  --     enable = true,
+  --     keymaps = {
+  --       init_selection = "<CR>",
+  --       node_incremental = "<CR>",
+  --       node_decremental = "<BS>",
+  --       scope_incremental = "<TAB>",
+  --     },
+  --   },
   -- 启用代码缩进模块 (=)
-  indent = {
-    enable = true,
-  },
+  -- indent = {
+  --   enable = true,
+  -- },
   -- p00f/nvim-ts-rainbow
   rainbow = {
     enable = true,
@@ -47,9 +47,3 @@ treesitter.setup({
     -- termcolors = { } -- table of colour name strings
   },
 })
--- 开启 Folding 模块
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
--- 默认不要折叠
--- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
-vim.opt.foldlevel = 99
